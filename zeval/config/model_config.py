97caf8e4f26494e93ae7886586a0b5411b35acd0
quentinzhang <tagriver@gmail.com>
2024-06-14 00:34:35 +0800

    Add lisense

--- zeval/config/model_config.py
+++
@@ -1,5 +0,0 @@
-# zeval/zeval/config/model_config.py
-class ModelConfig:
-    def __init__(self, provider, **model_kwargs):
-        self.provider = provider
-        self.model_kwargs = model_kwargs
--- zeval/evaluators/__init__.py
+++
@@ -1,7 +0,0 @@
-# zeval/zeval/evaluators/__init__.py
-from .faithfulness import Faithfulness
-from .answer_relevancy import AnswerRelevancy
-from .context_recall import ContextRecall
-from .context_precision import ContextPrecision
-from .matchness import Matchness
-from .json import IsJson, JsonMatch, JsonSchemaMatch
